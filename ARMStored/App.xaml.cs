95fe6b02438f1606e953f914a53c3428bfb095c9
slikovsky <pva8045@mail.ru>
2026-05-25 23:37:06 +0900

    Доработ очки

--- ARMStored/App.xaml.cs
+++ ARMStored/App.xaml.cs
@@ -1,4 +1,6 @@
+using System.Globalization;
 using System.Windows;
+using System.Windows.Markup;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ARMStored.Services;
@@ -13,6 +15,19 @@ public partial class App : Application
 
     private void OnStartup(object sender, StartupEventArgs e)
     {
+        // Устанавливаем русскую культуру для всего приложения
+        var russianCulture = new CultureInfo("ru-RU");
+        CultureInfo.CurrentCulture = russianCulture;
+        CultureInfo.CurrentUICulture = russianCulture;
+        CultureInfo.DefaultThreadCurrentCulture = russianCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = russianCulture;
+
+        // Устанавливаем культуру для XAML-привязок (важно для StringFormat=C)
+        FrameworkElement.LanguageProperty.OverrideMetadata(
+            typeof(FrameworkElement),
+            new FrameworkPropertyMetadata(
+                XmlLanguage.GetLanguage(russianCulture.IetfLanguageTag)));
+
         var services = new ServiceCollection();
         ConfigureServices(services);
         ServiceProvider = services.BuildServiceProvider();
